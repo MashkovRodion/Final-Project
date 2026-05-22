b04513af0799db7a13ba52f739dd03bbd6ff39a1
Rodion Mashkov <supersborshik125@gmail.com>
2026-05-22 17:48:36 +0300

    добавил настройки, предупреждаю код не совмещён пока что с предыдущей версией

---
+++ core/src/com/mygdx/game/Options.java
@@ -0,0 +1,9 @@
+package com.mygdx.game;
+
+public class Options {
+
+    public static float musicVolume = 1f;
+    public static float sfxVolume = 1f;
+    public static boolean fullscreen = false;
+
+}
