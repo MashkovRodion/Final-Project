0455dd77aa5416e151ee813de92c6589467f4b1a
flor1s-moreau <flop1s0680@gmail.com>
2026-06-08 20:00:30 +0300

    комментарии

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("FinalProject");
+		config.setTitle("Pixel Racing");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
